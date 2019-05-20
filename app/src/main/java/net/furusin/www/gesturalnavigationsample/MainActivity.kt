7dad4934691c39e3db41c19473ba408a9cd6803b
furusin <furusin.oriver@gmail.com>
2019-05-20 22:37:22 +0900

    ListViewの最後にNavigation Barの分のスペースを追加する

--- app/src/main/java/net/furusin/www/gesturalnavigationsample/MainActivity.kt
+++ app/src/main/java/net/furusin/www/gesturalnavigationsample/MainActivity.kt
@@ -2,17 +2,25 @@ package net.furusin.www.gesturalnavigationsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import androidx.core.view.ViewCompat
+import android.widget.ArrayAdapter
+import androidx.core.view.updatePadding
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         // フルスクリーンに対応する
         window.decorView.systemUiVisibility =
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
         val members = mutableListOf<String>()
         (0..100).forEach { members.add("furusin $it") }
 
@@ -21,6 +29,12 @@ class MainActivity : AppCompatActivity() {
                 this,
                 android.R.layout.simple_expandable_list_item_1, members
             )
+
+            // リストの最後にNavigation Barの分のスペースを追加する
+            listView.setOnApplyWindowInsetsListener { view, windowInsets ->
+                view.updatePadding(bottom = windowInsets.systemWindowInsetBottom)
+                windowInsets
+            }
         }
     }
 }
