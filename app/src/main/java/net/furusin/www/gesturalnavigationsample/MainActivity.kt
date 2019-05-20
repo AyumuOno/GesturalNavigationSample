54e4768f3bf072abb88e629b69c85690241212b5
furusin <furusin.oriver@gmail.com>
2019-05-20 22:36:24 +0900

    画面をフルスクリーン表示するように変更

--- app/src/main/java/net/furusin/www/gesturalnavigationsample/MainActivity.kt
+++ app/src/main/java/net/furusin/www/gesturalnavigationsample/MainActivity.kt
@@ -8,5 +8,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // フルスクリーンに対応する
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
     }
 }
